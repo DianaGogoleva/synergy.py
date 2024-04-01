724bc14307d6b71166aef42f5b3822bc7b16ca4c
DianaGogoleva <137498479+DianaGogoleva@users.noreply.github.com>
2024-04-01 10:30:57 +0500

    Update GogolevaHW15.1.py

--- GogolevaHW15.1.py
+++ GogolevaHW15.1.py
@@ -4,10 +4,7 @@ class Transport:
         self.max_speed = max_speed
         self.mileage = mileage
 
-class Autobus(Transport):
-    pass
-
-# Создаём объект Autobus
-autobus = Autobus('Renaul Logan', 180, 12)
+# Создаём объект Autobus с помощью класса Transport
+autobus = Transport('Renaul Logan', 180, 12)
 
 print('Название автомобиля:', autobus.name, 'Скорость:', autobus.max_speed, 'Пробег:', autobus.mileage)
