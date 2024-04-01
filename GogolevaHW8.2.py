70bb5d7206c00b6e6e89c6fd259cf314d4049527
DianaGogoleva <137498479+DianaGogoleva@users.noreply.github.com>
2024-04-01 10:14:09 +0500

    Update GogolevaHW8.2.py

--- GogolevaHW8.2.py
+++ GogolevaHW8.2.py
@@ -1,10 +1,8 @@
-N = int(input('Введите количество чисел:'))
-nums = list(map(int, input('Введите числа через пробел: ').split(' '))) 
-res = []
-for i in range(N):
-    if i % 2 == 0:
-        res.append(nums[-(i//2+1)])
-    else:
-        res.append(nums[i//2])
-for num in res:
-    print(num, end=" ")
+N = int(input('Введите количество элементов массива: '))
+array = list(map(int, input('Введите числа через пробел: ').split()))
+new_array = []
+new_array.append(array[-1])
+new_array.extend(array[:-1])
+
+for element in new_array:
+    print(element, end=' ')
