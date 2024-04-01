e61385079826264d1f14b2eb693caff59585d5e0
DianaGogoleva <137498479+DianaGogoleva@users.noreply.github.com>
2024-04-01 10:21:42 +0500

    Update GogolevaHW13.py

--- GogolevaHW13.py
+++ GogolevaHW13.py
@@ -1,17 +1,19 @@
 import random
 
-# Генерация двух матриц 10x10
-matrix_1 = [[random.randint(-200,200) for _ in range(10)] for _ in range(10)]
-matrix_2 = [[random.randint(-200,200) for _ in range(10)] for _ in range(10)]
+# Ввод размеров матрицы пользователем в формате rows * columns
+rows, columns = map(int,input("Введите размер матрицы в формате 'строки * столбцы': ").split('*'))
+
+# Генерация двух матриц rows x columns
+matrix_1 = [[random.randint(-200,200) for _ in range(columns)] for _ in range(rows)]
+matrix_2 = [[random.randint(-200,200) for _ in range(columns)] for _ in range(rows)]
 
 # Создание новой матрицы для результата
-matrix_3 = [[0 for _ in range(10)] for _ in range(10)]
+matrix_3 = [[0 for _ in range(columns)] for _ in range(rows)]
 
 # Сложение двух матриц
-for i in range(10):
-    for j in range(10):
+for i in range(rows):
+    for j in range(columns):
         matrix_3[i][j] = matrix_1[i][j] + matrix_2[i][j]
 
 # Печать результата
-print(matrix_3)
 print(matrix_3)
